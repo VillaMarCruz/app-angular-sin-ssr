f05f4616d9152d9806b762d5a14176c3e21f5b96
VillaMarCruz <marlonvillamar.12@gmail.com>
2024-03-13 13:42:04 -0500

    Start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+import { MenubarComponent } from './shared/menubar/menubar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenubarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app-angular-sin-ssr';
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MenubarComponent } from '../../shared/menubar/menubar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenubarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
